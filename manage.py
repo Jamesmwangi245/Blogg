1e167ae5b00bc0fc0e36e815d68ee946d6b346c4
jamesmwangi254 <james.kariuki@student.moringaschool.com>
2022-03-14 12:36:43 +0300

    manage

---
+++ manage.py
@@ -0,0 +1,30 @@
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User
+from flask_migrate import Migrate,MigrateCommand
+
+#creating app instances
+app = create_app('development')
+
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    """
+    run the unitests
+    """
+    import unittest
+    test = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(test)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User)
+
