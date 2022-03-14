032c7794c357f18bbf3dbbf7631e8d812566e3e7
jamesmwangi254 <james.kariuki@student.moringaschool.com>
2022-03-14 12:21:35 +0300

    model

---
+++ app/auth/forms.py
@@ -0,0 +1,38 @@
+from wtforms import validators
+from wtforms.validators import DataRequired,Email,EqualTo
+from wtforms import StringField, PasswordField, SubmitField, ValidationError,BooleanField
+from flask_wtf import FlaskForm
+from ..models import User
+
+
+class RegistrationForm(FlaskForm):
+    """
+    registration form class thats pass in the details for validation
+    """
+    email = StringField('your email address', validators=[DataRequired(), Email()])
+    username = StringField('your username', validators=[DataRequired()])
+    Password = PasswordField('password', validators=[DataRequired(), EqualTo('password', message='password must match')])
+    Password_confirm = PasswordField('confirm password', validators=[DataRequired()])
+    submit = SubmitField('sign up')
+
+
+
+    def validate_email(self,data_field):
+        """
+        functions that takes in the data field and checks the database to confirm user validation
+        """
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('that username is already taken or incorrect spellings')
+    
+
+class loginForm(FlaskForm):
+    email = StringField('your email address', validators=[DataRequired(),Email()])
+    password = StringField('password', validators=[DataRequired()])
+    remember = BooleanField('remeber me')
+    submit = SubmitField('Sign In')
+
---
+++ app/auth/views.py
@@ -0,0 +1,59 @@
+from flask import render_template, redirect, url_for, request , flash
+from flask_login import login_user, login_required, logout_user
+from .forms import RegistrationForm, loginForm
+from .import auth
+from ..import db
+from ..models import User
+
+
+
+#registration route
+@auth.route('templates/auth/reqister', methods=['GET','POST'])
+def register():
+    """
+    function that registers the users
+    """
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, passwird = form.Password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+    title = "registration"
+
+    return render_template('auth/register.html', RegistrationForm = form, title = title)
+
+
+
+#logging in function
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    """
+    function that checks if the form is validated
+    """
+
+    login_form = loginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next')or url_for('main.index'))
+
+
+            flash('Invalid Username or password')
+
+    title = "blogs"
+    return render_template('auth/login.html',login_form = login_form, title = title)
+
+
+#logging out function
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user
+    return redirect(url_for('main.index'))
+    #redirects user to the main page of the app after a successful login
+
+
